6e51aef0a21ff09958078c72a0c396e9ba81ec8c
Federico Cavanna <49650828+fedecavanna@users.noreply.github.com>
2023-08-18 17:14:11 -0300

    Agregué iteraciones por trials y guardado de resultados.

--- mid.py
+++ mid.py
@@ -1,101 +1,99 @@
+metadata dia hora ENOTCONN
+quitar seed
+gain acc debe restar no solo sumar
+
 import random
 from psychopy import visual, core, event, gui
 
-def populate_trial_data():
-    import random
-    trials = 10
-    seed_value = 42
-    random.seed(seed_value)
-
-    # defino las opciones posibles (0 o 1) y las probabilidades
-    options = [0, 1]
-    box_1_proba = [0.2, 0.8]
-    box_2_proba = [0.5, 0.5]
-    box_3_proba = [0.8, 0.2]
-
-    # genero los 10 datos con las probabilidades dadas
-    box_1 = random.choices(options, box_1_proba, k=trials)  
-    box_2 = random.choices(options, box_2_proba, k=trials)  
-    box_3 = random.choices(options, box_3_proba, k=trials)  
-    
-    result = []
-    for i in range(10):
-        result.append([box_1[i], box_2[i], box_3[i]])
-    
-    print('')
-
 class MonetaryIncentiveDelayTask:
 
-    def __init__(self, subject_id):
-        # Variables de instancia:
-        self.trials = 10
-        self.reward_probabilities_learn = [0.8, 0.5, 0.2]
-        self.reward_probabilities_reverse_learn = [0.2, 0.5, 0.8]
-        self.subject_name = subject_id
+    def populate_trial_data(self, n_trials, proba):
+        # genero los datos con un seed fijo
+        seed_value = 42 
+        random.seed(seed_value)
         
-        # visuales:
-        self.win = visual.Window(fullscr=True, 
-                                 # monitor='Dell precision',
-                                 allowGUI=False, 
-                                 color='gainsboro') # white
+        # defino la ocurrencia de cada box acorde a las probabilidades dadas
+        options = [0, 1]
+        box_1 = random.choices(options, proba[0], k=n_trials)
+        box_2 = random.choices(options, proba[1], k=n_trials)
+        box_3 = random.choices(options, proba[2], k=n_trials)
+        
+        # armo los n_trials por box basándome en el array calculado previamente
+        result = []
+        for i in range(10):
+            result.append([box_1[i], box_2[i], box_3[i]])
+        print(result)
+        return result
+
+    def __init__(self, subject_id):     
+        # defino el reward de los cofres:
+        self.trials = 10
+        self.learn_trial = self.populate_trial_data(self.trials, [[0.8, 0.2], [0.5, 0.5], [0.2, 0.8]])
+        self.reverse_trial = self.populate_trial_data(self.trials, [[0.2, 0.8], [0.5, 0.5], [0.8, 0.2]])        
+        
+        # defino variables visuales:
+        self.win = visual.Window(fullscr=False, allowGUI=False, color='gainsboro') # white
         self.fixation_cross = visual.TextStim(self.win, text='+', color='black', height=0.1)
         self.square_size = 0.2
         self.square_colors = ['gray', 'gray', 'gray']
         
-        # guardado:
+        # defino variables de guardado:
         self.trial_data = []
         self.results_file = f"results/{subject_id}.txt"
 
     def show_instructions(self):
-        instruction_text = "Presiona cualquier tecla para comenzar el experimento."
+        instruction_text = 'Presiona cualquier tecla para comenzar el experimento.'
         instructions = visual.TextStim(self.win, text=instruction_text, color='black')
         instructions.draw()
         self.win.flip()
         event.waitKeys()
 
     def run(self):
-        iti = 1 # intervalo entre trials
+        iti = 1 # intervalo entre trials (segundos)
         self.show_instructions()
-        for trial in range(self.trials):
-            reward_probability = random.choice(self.reward_probabilities)
-            self.run_trial(reward_probability)
+        for i in range(self.trials):
+            self.run_trial('learn', i+1, self.learn_trial[i]) # paso como parámetro el reward ya estimado de cada trial
             core.wait(iti)
             
         self.save_results() 
 
-    def run_trial(self, reward_probability):
+    def run_trial(self, cond, trial_n, trial_reward):
+        # creo la cruz de fijación
         self.fixation_cross.draw()
         self.win.flip()
-        core.wait(1)  # Fixation period
-
-        squares = self.create_squares(reward_probability)
+        core.wait(1) 
+        
+        # creo los cuadrados
+        self.create_squares()
         self.win.flip()
         start_time = core.getTime()
         while True:
             keys = event.waitKeys(keyList=['left', 'down', 'right'])
-            if keys[0] in ['left', 'down', 'right']:  # Check if a valid key was pressed
-                break
-            if core.getTime() - start_time > 5:
-                selection = 'NS'
+            if keys[0] in ['left', 'down', 'right']:
                 break
 
-        selected_square = ['left', 'down', 'right'].index(keys[0])
-        result_text = '+1' if random.randint(1, 100) <= int(reward_probability * 100) else '-1'
+        # evalúo si le pegaron al box
+        selected_square = ['left', 'down', 'right'].index(keys[0]) # devuelve 0, 1, 2
+        hit = trial_reward[selected_square] == 1 
+        result_text = '+1' if hit else '-1'
 
-        # Show feedback
+        # muestro el feedback
+        color = 'red'
         if result_text == '+1':
             color = 'green'
-        else:
-            color = 'red'
 
         feedback = visual.TextStim(self.win, text=result_text, color=color, height=0.1)
         feedback.draw()
         self.win.flip()
         core.wait(1)
 
-        self.trial_data.append((selected_square, int((core.getTime() - start_time) * 1000), result_text))
+        # acumulo el resultado
+        latency = int((core.getTime() - start_time) * 1000)
+        gain_acc = self.trial_data[-1][6] + int(hit) if len(self.trial_data) > 0 else int(hit)
+        streak = self.trial_data[-1][7] + 1 if len(self.trial_data) > 0 and hit else int(hit) 
+        self.trial_data.append([cond, trial_n, trial_reward, latency, selected_square, int(hit), gain_acc, streak])
 
-    def create_squares(self, reward_probability):
+    def create_squares(self):
         squares = []
         for i in range(3):
             square = visual.Rect(self.win, width=self.square_size, height=self.square_size, fillColor='gray')
@@ -109,20 +107,16 @@ class MonetaryIncentiveDelayTask:
 
     def save_results(self):
         with open(self.results_file, 'w') as f:
-            f.write("Trial_n;Latency_ms);box;hit\n")
+            f.write("cond;trial_n;trial_setup;latency_ms;selection;hit;gain_acc;streak\n")
             for i, data in enumerate(self.trial_data):
-                hit = 1 if data[0] == data[2] else 0
-                f.write(f"{i+1};{data[1]};{data[2]};{hit}\n")
+                f.write(f"{data[0]};{data[1]};{data[2]};{data[3]};{data[4]};{data[5]};{data[6]};{data[7]}\n")
 
-if __name__ == "__main__":
-    # Genero la información de los trials:
-    populate_trial_data()
-    
+if __name__ == "__main__": 
     # Pido toda información relevante que necesite:
     dlg = gui.Dlg(title="Información")
     dlg.addText("Por favor, ingresa el ID del sujeto:")
-    dlg.addField("Sujeto: ", "s00")
+    dlg.addField("Sujeto: ", "s0")
     subject_id = dlg.show()
     if dlg.OK:
-        task = MonetaryIncentiveDelayTask(subject_id)
+        task = MonetaryIncentiveDelayTask(subject_id[0])
         task.run()
