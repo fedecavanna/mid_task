7a50e48b0306567e5b4c90960cff51e617a22be7
Federico Cavanna <49650828+fedecavanna@users.noreply.github.com>
2023-09-26 11:35:22 -0300

    Comments translated. Readme file added.

---
+++ README.md
@@ -0,0 +1,48 @@
+
+# Monetary Incentive Delay Task (MID)
+
+This repository contains a Python implementation of the Monetary Incentive Delay Task (MID). The MID is a cognitive task that measures motivation and reward sensitivity. It is often used in research on decision-making, and reinforcement learning.
+
+## Usage
+
+To run the task, simply clone this repository and install the required dependencies:
+* StreamInfo: https://pypi.org/project/pylsl/
+* Psychopy: https://www.psychopy.org/
+```
+pip install pylsl psychopy
+```
+This will install the two required packages. Then, run the following command or run the code from your IDE:
+```
+python mid.py
+```
+You will be prompted to enter the Subject ID. Once you have entered it, the task will begin.
+
+## Data
+
+The task will save the results to a file called 'results/{subject_id}.txt'. 
+To date, this file will contain the following columns:
+
+* cond: The condition (learn or reverse)
+* trial_n: The trial number
+* trial_setup: The reward setup for the trial
+* latency_ms: The time it took the subject to respond
+* selection: The box that the subject selected
+* hit: Whether the subject selected the correct box
+* result: The subject's total earnings after the trial
+* streak: The subject's current streak of correct selections
+
+## EEG
+
+The task can also be used to record EEG data. To do this, you will need to connect an EEG device to your computer and use a recorder before running the task.
+
+To date, the task will stream the following EEG markers:
+
+* experiment_start
+* fixation_shown
+* key_pressed
+* feedback_shown
+* trial_start
+* trial_end
+
+You can use these markers to align your EEG data with the task events.
+ 
--- mid.py
+++ mid.py
@@ -1,23 +1,22 @@
-# agregar señalización al eeg
-
+# Necessary imports
 import random
 from psychopy import visual, core, event, gui
 from pylsl import StreamInfo, StreamOutlet
 
 class MonetaryIncentiveDelayTask:
     def populate_trial_data(self, n_trials, proba):
-        # genero los datos con un seed fijo
+        # Fixed seed for replication purposes
         seed_value = -1 
         if (seed_value > 0):            
             random.seed(seed_value)
         
-        # defino la ocurrencia de cada box acorde a las probabilidades dadas
+        # Define the occurrence of each box based on the given probabilities
         options = [0, 1]
         box_1 = random.choices(options, proba[0], k=n_trials)
         box_2 = random.choices(options, proba[1], k=n_trials)
         box_3 = random.choices(options, proba[2], k=n_trials)
         
-        # armo los n_trials por box basándome en el array calculado previamente
+        # Create the trial data based on the previously calculated array
         result = []
         for i in range(10):
             result.append([box_1[i], box_2[i], box_3[i]])
@@ -25,28 +24,28 @@ class MonetaryIncentiveDelayTask:
         return result
 
     def __init__(self, subject_id):     
-        # https://pypi.org/project/pylsl/
+        # Create an LSL stream for sending markers
         self.info = StreamInfo(name='MarkerStream', type='Markers', channel_count=1, 
                                 channel_format='string', source_id='myuidw43536')
         self.outlet = StreamOutlet(self.info)
         
-        # defino el reward de los cofres:
+        # Define the reward for the chests:
         self.trials = 10
         self.learn_trial = self.populate_trial_data(self.trials, [[0.8, 0.2], [0.5, 0.5], [0.2, 0.8]])
         self.reverse_trial = self.populate_trial_data(self.trials, [[0.2, 0.8], [0.5, 0.5], [0.8, 0.2]])        
         
-        # defino variables visuales:
+        # Define visual variables:
         self.win = visual.Window(fullscr=False, allowGUI=False, color='gainsboro') # white
         self.fixation_cross = visual.TextStim(self.win, text='+', color='black', height=0.1)
         self.square_size = 0.2
         self.square_colors = ['gray', 'gray', 'gray']
         
-        # defino variables de guardado:
+        # Define storage variables:
         self.trial_data = []
         self.results_file = f"results/{subject_id}.txt"
 
     def show_instructions(self):
-        instruction_text = 'Presiona cualquier tecla para comenzar el experimento.'
+        instruction_text = 'Press any key to start the experiment.'
         instructions = visual.TextStim(self.win, text=instruction_text, color='black')
         instructions.draw()
         self.win.flip()
@@ -54,33 +53,33 @@ class MonetaryIncentiveDelayTask:
 
     def run(self):
         self.outlet.push_sample('experiment_start') # EEG marker
-        iti = 1 # intervalo entre trials (segundos)
+        iti = 1 # Interval between trials (seconds)
         self.show_instructions()
         for i in range(self.trials):
-            self.run_trial('learn', i+1, self.learn_trial[i]) # paso como parámetro el reward ya estimado de cada trial
+            self.run_trial('learn', i+1, self.learn_trial[i]) # Pass the estimated reward of each trial as a parameter
             core.wait(iti)
         self.outlet.push_sample('experiment_end') # EEG marker
         self.save_results() 
 
     def run_trial(self, cond, trial_n, trial_reward):
         
-        # Manejo el contador con el resultado total de ganancias-pérdidas:
+        # Handle the counter for the total gain-loss result:
         def calc_result(self, hit): 
-            # obtengo el last count
+            # Get the last count
             n = self.trial_data[-1][6] if len(self.trial_data) > 0 else 0
-            # evalúo si agrego o resto y devuelvo
+            # Evaluate whether to add or subtract and return
             res = n+1 if hit == 1 else n-1 
             return res
         
         self.outlet.push_sample('trial_start') # EEG marker
         
-        # creo la cruz de fijación
+        # Create the fixation cross
         self.fixation_cross.draw()
         self.win.flip()
         self.outlet.push_sample('fixation_shown') # EEG marker
         core.wait(1) 
         
-        # creo los cuadrados
+        # Create the squares
         self.create_squares()
         self.win.flip()
         start_time = core.getTime()
@@ -91,12 +90,12 @@ class MonetaryIncentiveDelayTask:
                 self.outlet.push_sample('key_pressed') # EEG marker
                 break
 
-        # evalúo si le pegaron al box
-        selected_square = ['left', 'down', 'right'].index(keys[0]) # devuelve 0, 1, 2
+        # Check if they hit the box
+        selected_square = ['left', 'down', 'right'].index(keys[0]) # returns 0, 1, 2
         hit = trial_reward[selected_square]
         result_text = '+1' if hit else '-1'
 
-        # muestro el feedback
+        # Show feedback
         color = 'red'
         if result_text == '+1':
             color = 'green'
@@ -107,7 +106,7 @@ class MonetaryIncentiveDelayTask:
         self.outlet.push_sample('feedback_shown') # EEG marker
         core.wait(1)
 
-        # acumulo el resultado
+        # Accumulate the result
         result = calc_result(self, hit)
         streak = self.trial_data[-1][7] + 1 if len(self.trial_data) > 0 and bool(hit) else hit 
         self.trial_data.append([cond, trial_n, trial_reward, latency, selected_square, hit, result, streak])
@@ -134,7 +133,7 @@ class MonetaryIncentiveDelayTask:
                 f.write(f"{data[0]};{data[1]};{data[2]};{data[3]};{data[4]};{data[5]};{data[6]};{data[7]}\n")
 
 if __name__ == "__main__": 
-    # Pido toda información relevante que necesite:
+    # Request any relevant information needed:
     dlg = gui.Dlg(title="Información")
     dlg.addText("Por favor, ingresa el ID del sujeto:")
     dlg.addField("Sujeto: ", "s0")
