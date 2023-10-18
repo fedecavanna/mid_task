627520411da23ff674ccafea904f0c103e5092a0
Federico Cavanna <49650828+fedecavanna@users.noreply.github.com>
2023-10-18 16:24:45 -0300

    Modifications and corrections (times, conditions, messaging).

--- mid.py
+++ mid.py
@@ -4,6 +4,31 @@ from psychopy import visual, core, event, gui
 from pylsl import StreamInfo, StreamOutlet
 
 class MonetaryIncentiveDelayTask:
+
+    def __init__(self, subject_id):     
+        # Create an LSL stream for sending markers
+        self.info = StreamInfo(name='MarkerStream', type='Markers', channel_count=1, 
+                                channel_format='string', source_id='myuidw43536')
+        self.outlet = StreamOutlet(self.info)
+        
+        # Number of trials for each condition:
+        self.n_trials = 10
+        
+        # Define the reward for the chests:
+        # Each number in the probability set eg [0.8, 0.2] is the chance for each option [0, 1].
+        self.learn_trial = self.populate_trial_data(self.n_trials, [[0.8, 0.2], [0.5, 0.5], [0.2, 0.8]]) # right box > left box
+        self.reverse_trial = self.populate_trial_data(self.n_trials, [[0.2, 0.8], [0.5, 0.5], [0.8, 0.2]]) # left box > right box       
+        
+        # Define visual variables:
+        self.win = visual.Window(fullscr=True, allowGUI=False, color='gainsboro', monitor='2', screen=1) # white
+        self.fixation_cross = visual.TextStim(self.win, text='+', color='black', height=0.1)
+        self.square_size = 0.2
+        self.square_colors = ['gray', 'gray', 'gray']
+        
+        # Define storage variables:
+        self.trial_data = []
+        self.results_file = f"results/{subject_id}.txt"
+
     def populate_trial_data(self, n_trials, proba):
         # Fixed seed for replication purposes
         seed_value = -1 
@@ -23,45 +48,33 @@ class MonetaryIncentiveDelayTask:
         print(result)
         return result
 
-    def __init__(self, subject_id):     
-        # Create an LSL stream for sending markers
-        self.info = StreamInfo(name='MarkerStream', type='Markers', channel_count=1, 
-                                channel_format='string', source_id='myuidw43536')
-        self.outlet = StreamOutlet(self.info)
-        
-        # Define the reward for the chests:
-        self.trials = 10
-        self.learn_trial = self.populate_trial_data(self.trials, [[0.8, 0.2], [0.5, 0.5], [0.2, 0.8]])
-        self.reverse_trial = self.populate_trial_data(self.trials, [[0.2, 0.8], [0.5, 0.5], [0.8, 0.2]])        
-        
-        # Define visual variables:
-        self.win = visual.Window(fullscr=False, allowGUI=False, color='gainsboro') # white
-        self.fixation_cross = visual.TextStim(self.win, text='+', color='black', height=0.1)
-        self.square_size = 0.2
-        self.square_colors = ['gray', 'gray', 'gray']
-        
-        # Define storage variables:
-        self.trial_data = []
-        self.results_file = f"results/{subject_id}.txt"
-
-    def show_instructions(self):
-        instruction_text = 'Press any key to start the experiment.'
+    def show_text(self, text):
+        instruction_text = text
         instructions = visual.TextStim(self.win, text=instruction_text, color='black')
         instructions.draw()
         self.win.flip()
         event.waitKeys()
 
     def run(self):
-        self.outlet.push_sample('experiment_start') # EEG marker
-        iti = 1 # Interval between trials (seconds)
-        self.show_instructions()
-        for i in range(self.trials):
+        self.outlet.push_sample(['experiment_start']) # EEG marker
+        self.show_text('Instrucciones.\n\nPresioná cualquier tecla para comenzar el experimento.')
+        
+        for i in range(self.n_trials):
             self.run_trial('learn', i+1, self.learn_trial[i]) # Pass the estimated reward of each trial as a parameter
-            core.wait(iti)
-        self.outlet.push_sample('experiment_end') # EEG marker
+        for i in range(self.n_trials):
+            self.run_trial('reverse', i+1, self.reverse_trial[i]) # Pass the estimated reward of each trial as a parameter
+            
+        self.outlet.push_sample(['experiment_end']) # EEG marker
         self.save_results() 
 
     def run_trial(self, cond, trial_n, trial_reward):
+        # Each trial is performed as follows
+        # [fixation_stimuli + stimuli] [gap] [fixation_result + result] [iti]
+        
+        fixtime_stimuli = 2 # Time for the fixation cross pre stimuli. 
+        fixtime_result = 4 # Time for the fixation cross pre results.
+        gap_time = 0 # Time between stimuli and result blocks
+        iti_time = 2 # Interval between trials
         
         # Handle the counter for the total gain-loss result:
         def calc_result(self, hit): 
@@ -71,13 +84,14 @@ class MonetaryIncentiveDelayTask:
             res = n+1 if hit == 1 else n-1 
             return res
         
-        self.outlet.push_sample('trial_start') # EEG marker
+        self.outlet.push_sample(['trial_start']) # EEG marker
         
-        # Create the fixation cross
+        ## STIMULI BLOCK
+        # Create the fixation cross (pre stimuli)
         self.fixation_cross.draw()
         self.win.flip()
-        self.outlet.push_sample('fixation_shown') # EEG marker
-        core.wait(1) 
+        self.outlet.push_sample(['stimuli_fixation_shown']) # EEG marker
+        core.wait(fixtime_stimuli) 
         
         # Create the squares
         self.create_squares()
@@ -87,33 +101,49 @@ class MonetaryIncentiveDelayTask:
             keys = event.waitKeys(keyList=['left', 'down', 'right'])
             if keys[0] in ['left', 'down', 'right']:
                 latency = int((core.getTime() - start_time) * 1000)
-                self.outlet.push_sample('key_pressed') # EEG marker
+                self.outlet.push_sample(['key_pressed']) # EEG marker
                 break
+
+        ## GAP BLOCK
+        # Clear the screen 
+        self.win.flip()
+        core.wait(gap_time) 
 
         # Check if they hit the box
         selected_square = ['left', 'down', 'right'].index(keys[0]) # returns 0, 1, 2
         hit = trial_reward[selected_square]
         result_text = '+1' if hit else '-1'
 
+        ## RESULTS BLOCK
+        # Create the fixation cross (pre results)
+        self.fixation_cross.draw()
+        self.win.flip()
+        self.outlet.push_sample(['result_fixation_shown']) # EEG marker
+        core.wait(fixtime_result) 
+
         # Show feedback
         color = 'red'
         if result_text == '+1':
             color = 'green'
-
-        feedback = visual.TextStim(self.win, text=result_text, color=color, height=0.1)
+        feedback = visual.TextStim(self.win, text=result_text, color=color, height=0.3, bold=True)
         feedback.draw()
         self.win.flip()
-        self.outlet.push_sample('feedback_shown') # EEG marker
-        core.wait(1)
+        self.outlet.push_sample(['feedback_shown']) # EEG marker
 
         # Accumulate the result
         result = calc_result(self, hit)
         streak = self.trial_data[-1][7] + 1 if len(self.trial_data) > 0 and bool(hit) else hit 
         self.trial_data.append([cond, trial_n, trial_reward, latency, selected_square, hit, result, streak])
-        trial_data_str = ', '.join(self.trial_data)
+        trial_data_str = ', '.join(f"{cond};{trial_n};{trial_reward};{latency};{selected_square};{hit};{result};{streak}")
                        
-        self.outlet.push_sample(trial_data_str) # EEG marker 
-        self.outlet.push_sample('trial_end') # EEG marker 
+        self.outlet.push_sample([trial_data_str]) # EEG marker 
+        self.outlet.push_sample(['trial_end']) # EEG marker 
+        
+        ## ITI BLOCK
+        # Clear the screen 
+        core.wait(iti_time)
+        self.show_text('Continuar')
+        
     def create_squares(self):
         squares = []
         for i in range(3):
@@ -136,7 +166,7 @@ if __name__ == "__main__":
     # Request any relevant information needed:
     dlg = gui.Dlg(title="Información")
     dlg.addText("Por favor, ingresa el ID del sujeto:")
-    dlg.addField("Sujeto: ", "s0")
+    dlg.addField("Sujeto: ", "s")
     subject_id = dlg.show()
     if dlg.OK:
         task = MonetaryIncentiveDelayTask(subject_id[0])
